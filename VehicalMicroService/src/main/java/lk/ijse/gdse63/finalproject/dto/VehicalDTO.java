85daa3d4e16645e7289e011d34139dd8c68a182a
Deshan001 <venomassassin01@gmail.com>
2023-10-12 22:44:25 +0530

    update vehical module

---
+++ VehicalMicroService/src/main/java/lk/ijse/gdse63/finalproject/dto/VehicalDTO.java
@@ -0,0 +1,31 @@
+package lk.ijse.gdse63.finalproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class VehicalDTO {
+        private int id;
+        private String name;
+        private String transmition;
+        private String category;
+        private String vehicalType;
+        private String seatCapasity;
+        private String fuelType;
+        private Double feeForDay;
+        private Double duelUsage;
+        private Double feeFor1km ;
+        private Boolean hybrid ;
+        private String remarks;
+        private String images;
+        private int driverId;
+        private String driverName;
+        private String contactNo;
+        private String licence;
+
+
+}
