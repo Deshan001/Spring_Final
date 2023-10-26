e484230bf70bae0ca1c33dcf3363c55fa8565d22
Deshan001 <venomassassin01@gmail.com>
2023-10-26 22:15:53 +0530

    Updating

--- GuideMicroService/src/main/java/lk/ijse/gdse63/finalproject/config/JwtAuthorizationFilter.java
+++ GuideMicroService/src/main/java/lk/ijse/gdse63/finalproject/config/JwtAuthorizationFilter.java
@@ -1,18 +1,18 @@
 package lk.ijse.gdse63.finalproject.config;
 
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class JwtAuthorizationFilter extends OncePerRequestFilter {
+public class JwtAuthorizationFilter {
     private final JwtUtil jwtUtil;
     private final ObjectMapper mapper;
 
@@ -66,4 +66,5 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         }
         filterChain.doFilter(request, response);
     }
+
 }
