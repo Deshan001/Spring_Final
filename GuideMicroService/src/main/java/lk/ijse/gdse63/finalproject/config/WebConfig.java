d8c83706005f6c9e6f194a16da0737b4ab6052b3
Deshan001 <venomassassin01@gmail.com>
2023-10-26 19:51:42 +0530

    new update

---
+++ GuideMicroService/src/main/java/lk/ijse/gdse63/finalproject/config/WebConfig.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse63.finalproject.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.convert.Jsr310Converters;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new Jsr310Converters.StringToLocalDateConverter());
+    }
+}
