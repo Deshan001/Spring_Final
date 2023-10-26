51d01e7582e7fd12933c5bf75fcb84e1bae37e45
Deshan001 <venomassassin01@gmail.com>
2023-10-26 19:21:08 +0530

    Authenticate Dependencies Added

--- AuthenticationMicroService/src/main/java/lk/ijse/gdse63/finalproject/service/impl/AdminServiceImpl.java
+++ AuthenticationMicroService/src/main/java/lk/ijse/gdse63/finalproject/service/impl/AdminServiceImpl.java
@@ -1,10 +1,11 @@
 package lk.ijse.gdse63.finalproject.service.impl;
 
-
 import lk.ijse.gdse63.finalproject.dto.AdminDTO;
 import lk.ijse.gdse63.finalproject.entity.Admin;
 import lk.ijse.gdse63.finalproject.repo.AdminRepo;
 import lk.ijse.gdse63.finalproject.service.AdminService;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,26 +16,24 @@ import java.util.List;
 
 @Service
 public class AdminServiceImpl implements AdminService {
-    private final AdminRepo adminRepository;
 
+    private final AdminRepo adminRepository;
     private final ModelMapper modelMapper;
 
-    public AdminServiceImpl(AdminRepo adminRepository, ModelMapper modelMapper){
+    public AdminServiceImpl(AdminRepo adminRepository, ModelMapper modelMapper) {
         this.adminRepository = adminRepository;
         this.modelMapper = modelMapper;
-
     }
-
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Admin user = adminRepository.findByEmail(email);
+        Admin admin = adminRepository.findByEmail(email);
         List<String> roles = new ArrayList<>();
         roles.add("USER");
         UserDetails userDetails =
                 org.springframework.security.core.userdetails.User.builder()
-                        .username(user.getEmail())
-                        .password(user.getPassword())
+                        .username(admin.getEmail())
+                        .password(admin.getPassword())
                         .roles(roles.toArray(new String[0]))
                         .build();
         return userDetails;
@@ -44,6 +43,23 @@ public class AdminServiceImpl implements AdminService {
     public AdminDTO searchUser(String email) {
         Admin byEmail = adminRepository.findByEmail(email);
         System.out.println(byEmail);
-        return modelMapper.map(byEmail,AdminDTO.class);
+        AdminDTO map = modelMapper.map(byEmail, AdminDTO.class);
+        //modelMapper.map(byEmail.getType(),new ArrayList<String>().getClass().getSuperclass());
+        ArrayList<String> strings = jsonStringToArray(byEmail.getType());
+        map.setType(strings);
+        return map;
+    }
+
+    ArrayList<String> jsonStringToArray(String jsonString) throws JSONException {
+
+        ArrayList<String> stringArray = new ArrayList<String>();
+
+        JSONArray jsonArray = new JSONArray(jsonString);
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            stringArray.add(jsonArray.getString(i));
+        }
+
+        return stringArray;
     }
 }
