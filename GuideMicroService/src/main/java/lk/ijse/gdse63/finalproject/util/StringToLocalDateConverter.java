839e344dfd8f746fa37f166836e9f03c86e934f7
Deshan001 <venomassassin01@gmail.com>
2023-10-26 22:06:15 +0530

    Converter

---
+++ GuideMicroService/src/main/java/lk/ijse/gdse63/finalproject/util/StringToLocalDateConverter.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse63.finalproject.util;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.time.LocalDate;
+
+public class StringToLocalDateConverter implements Converter<String, LocalDate> {
+
+
+    @Override
+    public LocalDate convert(String source) {
+        return LocalDate.parse(source);
+    }
+}
