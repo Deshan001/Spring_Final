68ebcb9d27b1f906154715ac1d3ef186640bb2d1
Deshan001 <venomassassin01@gmail.com>
2023-10-27 12:52:26 +0530

    DriverRepo Done

---
+++ VehicalMicroService/src/main/java/lk/ijse/gdse63/finalproject/repo/DriverRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse63.finalproject.repo;
+
+import lk.ijse.gdse63.finalproject.entity.Driver;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DriverRepo extends CrudRepository<Driver,Integer> {
+}
