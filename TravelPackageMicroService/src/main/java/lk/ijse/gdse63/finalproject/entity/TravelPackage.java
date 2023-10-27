52c7b3dc9b321554bfcd8d5b5a5ae795c8463e6a
Deshan001 <venomassassin01@gmail.com>
2023-10-27 14:33:53 +0530

    TravelPackage Finished

---
+++ TravelPackageMicroService/src/main/java/lk/ijse/gdse63/finalproject/entity/TravelPackage.java
@@ -0,0 +1,19 @@
+package lk.ijse.gdse63.finalproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TravelPackage extends SuperEntity{
+    @Id
+    private String id;
+    private int hotelCount;
+    private int areaCount;
+    private double estimatedPrice;
+    private String category;
+    private int dayCount;
+}
