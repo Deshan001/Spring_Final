3847d555505823e34d13d88d3411c661498ae6db
Deshan001 <venomassassin01@gmail.com>
2023-10-27 21:03:05 +0530

    AppInitializer Updated

--- UserTravelPackageMicroService/src/main/java/lk/ijse/gdse63/usertravelpackagemicroservice/UserTravelPackageMicroServiceApplication.java
+++ UserTravelPackageMicroService/src/main/java/lk/ijse/gdse63/usertravelpackagemicroservice/UserTravelPackageMicroServiceApplication.java
@@ -1,13 +1,21 @@
 package lk.ijse.gdse63.usertravelpackagemicroservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class UserTravelPackageMicroServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserTravelPackageMicroServiceApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
     }
 
 }
