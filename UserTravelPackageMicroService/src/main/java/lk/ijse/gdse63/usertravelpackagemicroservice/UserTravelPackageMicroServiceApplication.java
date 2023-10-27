58b2627a642cb247d01fe6da6797e9cd9092dedc
Deshan001 <venomassassin01@gmail.com>
2023-10-27 20:32:45 +0530

    MicroServiceAppInitializer Created

---
+++ UserTravelPackageMicroService/src/main/java/lk/ijse/gdse63/usertravelpackagemicroservice/UserTravelPackageMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse63.usertravelpackagemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTravelPackageMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserTravelPackageMicroServiceApplication.class, args);
+    }
+
+}
