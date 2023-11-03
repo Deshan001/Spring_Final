b4fbee959fdfee1f355e2f4a31706c6b5a48b910
Deshan001 <venomassassin01@gmail.com>
2023-11-03 10:02:49 +0530

    TravelPackageApi new Update

--- TravelPackageMicroService/src/main/java/lk/ijse/gdse63/finalproject/api/TravelPackageApi.java
+++ TravelPackageMicroService/src/main/java/lk/ijse/gdse63/finalproject/api/TravelPackageApi.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/travel-package")
+@CrossOrigin
 public class TravelPackageApi {
     TravelPackageService service;
     public TravelPackageApi(TravelPackageService service){
@@ -31,7 +32,7 @@ public class TravelPackageApi {
 
     }
 
-    @GetMapping("/{id:^NEXT-\\d{4}$}")
+    @GetMapping("/{id:^NEXT-\\d{5}$}")
     public ResponseEntity get(@PathVariable String id){
         try {
             TravelPackageDTO travelPackageDTO = service.fidById(id);
@@ -63,7 +64,7 @@ public class TravelPackageApi {
 
     }
 
-    @DeleteMapping("/{id:^NEXT-\\d{4}$}")
+    @DeleteMapping("/{id:^NEXT-\\d{5}$}")
     public ResponseEntity delete(@PathVariable String id){
         try {
             service.delete(id);
